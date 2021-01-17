37caaddfeda85ccbf8159964d01e28cde9515cb0
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-01-17 09:18:01 -0300

    docs(readme): create readme infos about this repo

---
+++ README.md
@@ -0,0 +1,22 @@
+# Desafio de lógica número 2
+
+> Dado um array de inteiros, crie uma função que retorne um novo array com o primeiro e último elemento do array dado na última e primeira posição do novo array, respectivamente.
+
+**Importante:** O array dado não pode ser modificado.
+
+Exemplos:
+
+**Entrada:** `[1, 2, 3]`
+**Saída:** `[3, 2, 1]`
+
+**Entrada:** `[12, 35, 9, 56, 24]`
+**Saída:** `[24, 35, 9, 56, 12]`
+
+-------------
+
+### Desafio concluído!
+
+Para testar dê um: `npm install`,\
+Em seguida: `npm run test`
+
+![print](/prits/print-1.png)
--- tests/script.test.js
+++ tests/script.test.js
@@ -1,6 +1,26 @@
 const manipulateNumbers = require('../script');
 
-test('should be null', () => {
-  expect(manipulateNumbers(null)).toBeNull();
-  expect(manipulateNumbers([])).toBeNull();
+describes("Test manipulateNumbers function", () => {
+
+    test('should be null when no values has been passed', () => {
+        expect(manipulateNumbers()).toBeNull();
+        expect(manipulateNumbers(null)).toBeNull();
+    });
+    
+    test('should be null when an empty array has been passed', () => {
+        expect(manipulateNumbers([])).toBeNull();
+    });
+    
+    test('should be null when an array of two elements has been passed', () => {
+        expect(manipulateNumbers([1, 2])).toBeNull();
+    });
+    
+    test('should manipulate an array of three numbers', () => {
+        expect(manipulateNumbers([1, 2, 3])).toStrictEqual([3, 2, 1]);
+    });
+    
+    test('should manipulate an array with many numbers', () => {
+        expect(manipulateNumbers([1, 2, 3, 4, 5, 6])).toStrictEqual([6, 2, 3, 4, 5, 1]);
+    });
+    
 });
